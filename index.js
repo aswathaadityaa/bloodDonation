82aac0125a8c2befc10dfa14147d3ed4f607d39f
aswathaadityaa.cs21 <aswathaadityaa.cs21@bitsathy.ac.in>
2025-03-16 17:38:19 +0530

    addedExpress

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port);
